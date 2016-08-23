f8b94e8d40ef4819cd063c010868c6321e7c27d0
xiaovcn <xiaov.cn@163.com>
2016-08-23 16:18:53 +0800

    fix jquery

--- app/assets/js/pages/settings.js
+++ app/assets/js/pages/settings.js
@@ -2,60 +2,59 @@ const config = require('electron-json-storage');
 
 let json;
 
-config.get('config', function(error, data) {
-        json =data;
+config.get('config', function (error, data) {
+    if (error) throw error;
+    json = data; 
+    init();
+    bindSaveEvent();
 
-        createconfig();
-
-        if (error) throw error;  
-
-        init();
-
-        bindSaveEvent();  
-        
 });
 
-function createconfig(){
-    if(!config.has('configs')){
+function createconfig() {
+    if (!config.has('configs')) {
         //create config in userdata
         json = {
             "configs": [
                 {
-                    "name":"东营市国税局",
-                    "loginurl": "http://www.baidu.com:8999/",            
-                    "username":"username",
-                    "password": "boy",   
-                    "plugins":[
+                    "name": "东营市国税局",
+                    "loginurl": "http://www.baidu.com:8999/",
+                    "username": "username",
+                    "password": "boy",
+                    "plugins": [
                         {
-                            "name":"plugin_0",
-                            "on":true                    
-                        },{
-                            "name":"plugin_1",
-                            "on":true
+                            "name": "plugin_0",
+                            "on": true
+                        }, {
+                            "name": "plugin_1",
+                            "on": true
                         }
-                    ]            
+                    ]
                 }
             ],
-            "notify": "true",
-            "system":"true"            
+            "notify": true,
+            "system": true
         }
 
-        config.set('config',json,null);
+        config.set('config', json, null);
     }
 }
 
 
-function init(){
+function init() {
+    if (json.configs==undefined) {
+
+        createconfig();
+    }
     //init data
-    $('#cb_system').prop("checked",json.system?"checked":"");
-    $('#cb_notify').prop("checked",json.notify?"checked":"");
+    $('#cb_system').prop("checked", json.system ? "checked" : "");
+    $('#cb_notify').prop("checked", json.notify ? "checked" : "");
     $('#loginurl').val(json.configs[0].loginurl);
     $('#username').val(json.configs[0].username);
     $('#password').val(json.configs[0].password);
     var arr = json.configs[0].plugins;
-    for(var i=0;i<arr.length;i++){
-        $('#plugins input').eq(i).prop("checked",json.configs[0].plugins[i].on?"checked":"");
-    } 
+    for (var i = 0; i < arr.length; i++) {
+        $('#plugins input').eq(i).prop("checked", json.configs[0].plugins[i].on ? "checked" : "");
+    }
 
 
     //stled
@@ -64,20 +63,20 @@ function init(){
     });
 
 }
-function bindSaveEvent(){
+function bindSaveEvent() {
 
-     //开机启动
-    $('#cb_system').click(function(){      
-        json.system=$('#cb_system').is(':checked'); 
-        config.set('config', json, function(error) {
+    //开机启动
+    $('#cb_system').click(function () {
+        json.system = $('#cb_system').is(':checked');
+        config.set('config', json, function (error) {
             if (error) throw error;
         });
     })
 
     //弹窗提醒
-    $('#cb_notify').click(function(){      
-        json.notify=$('#cb_notify').is(':checked'); 
-        config.set('config', json, function(error) {
+    $('#cb_notify').click(function () {
+        json.notify = $('#cb_notify').is(':checked');
+        config.set('config', json, function (error) {
             if (error) throw error;
         });
     })
@@ -85,39 +84,39 @@ function bindSaveEvent(){
     //东营市国税局
     //登录url
     var obj = json.configs[0];
-    $('#loginurl').blur(function(){      
-        obj.loginurl=$('#loginurl').val(); 
-        
-        config.set('config', json, function(error) {
+    $('#loginurl').blur(function () {
+        obj.loginurl = $('#loginurl').val();
+
+        config.set('config', json, function (error) {
             if (error) throw error;
         });
     })
 
     //用户名
-    $('#username').blur(function(){      
-        obj.username=$('#username').val(); 
-        
-        config.set('config', json, function(error) {
+    $('#username').blur(function () {
+        obj.username = $('#username').val();
+
+        config.set('config', json, function (error) {
             if (error) throw error;
         });
     })
 
     //密码
-    $('#password').blur(function(){      
-        obj.password=$('#password').val(); 
-        
-        config.set('config', json, function(error) {
+    $('#password').blur(function () {
+        obj.password = $('#password').val();
+
+        config.set('config', json, function (error) {
             if (error) throw error;
         });
     })
 
-     //插件
-    $('#plugins input').click(function(){
+    //插件
+    $('#plugins input').click(function () {
         var id = this.id;
         var index = id.split('_')[1];
         //console.log(index+'_'+$(this).is(':checked'));
-        obj.plugins[index].on=$(this).is(':checked');
-        config.set('config', json, function(error) {
+        obj.plugins[index].on = $(this).is(':checked');
+        config.set('config', json, function (error) {
             if (error) throw error;
         });
     })
--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const electron = require('electron')
 const app = electron.app
-//const fs = require('fs')
+const fs = require('fs')
 const path = require('path')
 const Tray = electron.Tray
 const Menu = electron.Menu
@@ -21,9 +21,9 @@ app.on('ready', () => {
   //加载主窗口
   createWindow()
   //任务栏
-  createTray()
+  //createTray()
   //加载弹出窗口
-  createPopWindow()
+  //createPopWindow()
 })
 
 //主窗口
@@ -31,8 +31,8 @@ function createWindow() {
 
   if (!win) {
     win = new BrowserWindow({ width: 980, height: 600, frame: true, resizable: true, icon: path.join(__dirname, '', 'assets/images/icon.png') })
-    //win.webContents.openDevTools()
-    win.loadURL(`file://${__dirname}/index.html`)
+    win.webContents.openDevTools()
+    win.loadURL(`file://${__dirname}/settings.html`)
     
     win.on('minimize', () => {
       win.hide()
